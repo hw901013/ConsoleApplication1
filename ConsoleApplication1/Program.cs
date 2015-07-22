a5e71201268aa366a3cc8183e4ffed1d5fc6c0b9
黄伟 <hw901013@sina.com>
2015-07-22 21:38:56 +0800

    5

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
@@ -10,6 +12,28 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string host = "localhost";
+            string elementKey = "testKeyRedis";
+
+            using (RedisClient redisClient = new RedisClient(host))
+            {
+                if (redisClient.Get<string>(elementKey) == null)
+                {
+                    // adding delay to see the difference
+                    Thread.Sleep(2000);
+                    // save value in cache
+                    redisClient.Set(elementKey, "default value");
+                }
+
+                //change the value
+                redisClient.Set(elementKey, "fuck you value");
+
+                // get value from the cache by key
+                string message = "Item value is: " + redisClient.Get<string>(elementKey);
+
+                Console.WriteLine(message);
+                Console.ReadLine();
+            }
         }
     }
 }
